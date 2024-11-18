c395f504111ea220eda0cf3d544fac5950496f5a
Frejo004 <frejohope@gmail.com>
2024-11-18 18:25:22 +0100

    installation des router

--- public/index.php
+++ public/index.php
@@ -1,3 +1,22 @@
 <?php
-    var_dump("hello word !");
-    // phpinfo()
+
+use App\Router;
+
+    require __DIR__ . "/../vendor/autoload.php";
+
+    //Démarrer la session
+    if(isset($_SESSION)){
+        session_start();
+    }
+
+    //Créer l'instance de route
+    $router = new Router();
+
+    $router -> get ("/", function(){});
+    $router -> get ("/add", function(){});
+    $router -> post ("/add", function(){});
+    $router -> get ("/toggle", function(){});
+    $router -> get ("/delete", function(){});
+
+    echo"<pre>";
+    var_dump($router);
