7c1ce9497969856e9f317ecb01491689f07be1e0
Frejo004 <frejohope@gmail.com>
2024-11-19 12:32:32 +0100

    action et méthode sur la page

--- src/views/add.php
+++ src/views/add.php
@@ -1,6 +1,6 @@
 <h1>Ajouter une nouvelle tâche</h1>
 
-<form action="">
+<form action="/add" method="post">
     <input type="text" name="task" placeholder="Entrez uune tâche" required>
     <button type="submit">Ajouter</button>
 </form>
