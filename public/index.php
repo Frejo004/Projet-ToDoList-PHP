faaa7d8642bad53a41771e871062b641ec950bf3
Frejo004 <frejohope@gmail.com>
2024-11-19 11:06:36 +0100

    Définir les router de l'application

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\TodoController;
 use App\Router;
 
     require __DIR__ . "/../vendor/autoload.php";
@@ -12,11 +13,18 @@ use App\Router;
     //Créer l'instance de route
     $router = new Router();
 
-    $router -> get ("/", function(){});
-    $router -> get ("/add", function(){});
-    $router -> post ("/add", function(){});
-    $router -> get ("/toggle", function(){});
-    $router -> get ("/delete", function(){});
+    //Crérer une instance de contrôllers
+    $todoController = new TodoController();
 
+    //Définir les router de l'application
+    $router -> get ("/", [$todoController, 'index' ]);
+    $router -> get ("/add", [$todoController, 'add' ]);
+    $router -> post ("/add", [$todoController, 'add' ]);
+    $router -> get ("/toggle", [$todoController, 'toggle' ]);
+    $router -> get ("/delete", [$todoController, 'delete' ]);
+
+
+    //Résoudre ma route correspondant
+    $router -> resolve();
     echo"<pre>";
     var_dump($router);
