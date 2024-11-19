17d23f2fa5f16195a617f45afaea2798ab74907b
Frejo004 <frejohope@gmail.com>
2024-11-19 14:41:16 +0100

    fun ajout and delete

--- src/Controllers/TodoController.php
+++ src/Controllers/TodoController.php
@@ -10,22 +10,27 @@
 
             $todos = $_SESSION["todos"] ?? [];//si $todos est NULL, on prend le tableau vide
 
+            // echo "<pre>";
+            // echo session_save_path();
+            //  print_r($todos);
+            // var_dump($todos);
+
+
             //Charger la vue "Views/index.php"
             require __DIR__ . "/../Views/index.php";
         }
 
         public function add(){
-            if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            if($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $task = trim($_POST['task']);
-
-                if($task){
+                if($task) {
                     $_SESSION['todos'][] = [
-                        'id' => uniqid("todo_"),
+                        'id' => uniqid('todo_'),
                         'task' => $task,
-                        'done' => false
+                        'done' => false,
                     ];
                 }
-                header('location : /');
+                header('Location: /');
                 exit;
             }
             //charger la vue add.php
@@ -33,10 +38,27 @@
         }
 
         public function delete(){
+            $id = $_GET['id'] ?? null;
+            if($id){
+                $_SESSION['todos'] = array_filter($_SESSION['todos'], function($todo) use ($id){
+                    return $todo['id'] !== $id;
+                });
+            }
 
+            header('Location: /');
+            exit;
         }
 
         public function toggle(){
-
+            $id = $_GET['id'] ?? null;
+            if($id){
+                foreach($_SESSION['todos'] as &$todo){
+                    if($todo['id'] === $id){
+                        $todo['done'] = !$todo['done'];
+                    }
+                }
+            }
+            header('Location: /');
+            exit;
         }
     }
