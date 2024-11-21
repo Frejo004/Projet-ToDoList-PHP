d4f183ba69e80011e17d3f742bc149e97ea77ec6
Frejo004 <frejohope@gmail.com>
2024-11-21 09:45:20 +0100

    les routes post/get de update

--- public/index.php
+++ public/index.php
@@ -20,6 +20,8 @@ use App\Router;
     $router -> get ("/", [$todoController, 'index' ]);
     $router -> get ("/add", [$todoController, 'add' ]);
     $router -> post ("/add", [$todoController, 'add' ]);
+    $router -> get ("/update", [$todoController, 'update' ]);
+    $router -> post ("/update", [$todoController, 'update' ]);
     $router -> get ("/toggle", [$todoController, 'toggle' ]);
     $router -> get ("/delete", [$todoController, 'delete' ]);
 
