dc0e60e4fbccb807f6c2a2e3c121ebe3cf4e9c8a
Frejo004 <frejohope@gmail.com>
2024-11-21 09:46:26 +0100

    lien sur le home pour le update

--- src/views/index.php
+++ src/views/index.php
@@ -5,12 +5,13 @@
 <a href="/add">Ajouter une nouvelle tâche</a>
 <ul>
     <?php foreach($todos as $todo):?>
-    <li>
         <span style="text-decoration: <?= $todo['done'] ? 'line-through' : 'none' ?>">
             <?= htmlspecialchars($todo['task']) ?>
         </span>
+
         <a href="/toggle?id=<?= $todo['id'] ?>" >✅</a>
         <a href="/delete?id=<?= $todo['id'] ?>">❌</a>
+        <a href="/update?id=<?= $todo['id'] ?>">🖊</a>
     </li>
     <?php endforeach ?>
 </ul>
