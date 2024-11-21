198edc1be0f95e80f9c58a17e008249126b8116d
Frejo004 <frejohope@gmail.com>
2024-11-21 18:09:26 +0100

    Héritage pour utilisation de la BDD

---
+++ src/Models/Model.php
@@ -0,0 +1,13 @@
+<?php
+    namespace App\Models;
+
+    use DB\Database;
+
+    class Model{
+        protected $db;
+
+        public function __construct(){
+            $this -> db = Database::getInstance();
+        }
+            
+    }
